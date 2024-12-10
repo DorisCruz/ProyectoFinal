3359f7b31cd640959c6dc97c1cba6482a3edfb42
doris Cruz <dbcruzhdez@gmail.com>
2024-12-10 07:29:51 -0800

    Binaria

--- EDDProy/Recursividad/Clases/Binaria.cs
+++ EDDProy/Recursividad/Clases/Binaria.cs
@@ -11,33 +11,28 @@ namespace EDDemo
       
             public int Buscar(int[] arreglo, int objetivo, int inicio = 0, int fin = -1)
             {
-            if (fin == -1)
+            if (fin == 0)
             {
                 fin = arreglo.Length - 1;
             }
 
-            // Condición base: si los límites son inválidos, retornar -1
             if (inicio > fin)
             {
                 return -1;
             }
 
-            // Calcular el índice del punto medio
             int medio = inicio + (fin - inicio) / 2;
 
-            // Si encontramos el elemento, devolver índice
             if (arreglo[medio] == objetivo)
             {
                 return medio;
             }
 
-            // Buscar en la mitad izquierda
             if (arreglo[medio] > objetivo)
             {
                 return Buscar(arreglo, objetivo, inicio, medio - 1);
             }
 
-            // Buscar en la mitad derecha
             return Buscar(arreglo, objetivo, medio + 1, fin);
         }
     
